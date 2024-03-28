6c11dfc3163bf21bfa1c325e31777b68f8a7a8ec
npmmew <npmmew20@gmail.com>
2024-03-28 22:12:33 +0300

    Изменение логотипа

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
+import logo from '../assets/logo.svg'
+
 function LoginForm() {
+    const [emailError, setEmailError] = useState('поле Email не может быть пустым')
+    const [passwordError, setPasswordError] = useState('поле Паоль не может быть пустым')
+
     return (
       <div className="h-screen w-screen flex items-center justify-center">
           <form action="" className="flex flex-col w-[400px] p-10 shadow-2xl">
+            <img src={logo} width={60} className="mx-auto" alt="" />
             <h1 className="font-medium text-3xl mb-6 text-center">Автoризация</h1>
-            <input className="rounded p-3 border mb-3" name="email" type="text" placeholder="Введите свой email"/>
-            <input className="rounded p-3 border " name="password" type="password" placeholder="Введите свой пароль"/>
+            <input className="rounded p-3 border mb-3 focus:border-blue-700 focus:border-2" name="email" type="text" placeholder="Введите свой email"/>
+            <input className="rounded p-3 border focus:border-blue-700 focus:border-2" name="password" type="password" placeholder="Введите свой пароль"/>
             <a className="float-left text-blue-700 hover:underline" href="">Забыли пароль?</a>
             <button className="rounded w-full p-3 bg-blue-700 text-white mt-3 hover:bg-blue-800 ease-in-out duration-300" type="submit">Войти</button>
             <p>Нужна учётная запись? <a className="text-blue-700 hover:underline" href="">Зарегистрируйтесь</a></p>
